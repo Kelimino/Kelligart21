43e91d45c341cf67ebebc5c0af4db633e019e2bd
kelimino <jourdren.kellig@gmail.com>
2021-02-10 19:21:43 +0100

    init

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,59 @@
   <div class="container">
     <div>
       <Logo />
+      <!-- function afternoon is a string (data dans array)  -->
+      <h3>{{ good("night") }}</h3>
       <h1 class="title">
-        kellig21
+        {{ presentation() }}
       </h1>
-      <lottie-animation path="icons/arrow.json" />
+      <!-- juste un crochet car c'est un objet et pas une value  -->
+      <div v-bind:class="{ active: active }" @click="clickHello">
+        <span>Hello world</span>
+      </div>
+      <p>{{ intro }}</p>
+      <!-- v-html fait passer une balise html, start est la data -->
+      <p v-html="start"></p>
+      <!-- vue js sait que le on:clikc appel un code donc pas de ()-->
+      <!-- v-on:click = @click -->
+      <div class="game">
+  <div v-bind:style="{ width:ballon + '%', height:ballon + '%', }" v-bind:class="{ballon, done: end} "></div>
+  <div class="air"><div v-bind:style="{ width:grow + '%', opacity:grow + '%' }"></div></div>
+  <button @click="blow" v-show="!end"> Souffler</button>
+  <button @click="restart">recommencer</button>
 
+
+      </div>
+      <button @click.once="add(1)">Add</button>
+      <button @click="minus(1)">Subtract</button>
+      <button @dblclick="add(10)">Add 10</button>
+      <button @dblclick="minus(10)">Subtract 10</button>
+      <p>my age is {{ age }}</p>
+      <div id="canvas" v-on:mousemove="updateXY">{{ x }}, {{ y }}</div>
+      <a @click.prevent="click" href="http://www.kelligart.com/" target="_blank"
+        >kelligart</a
+      >
+      <div>
+        <ul>
+          <li v-for="(pers, index) in personnage">
+            {{ pers }}
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li v-for="(lists, index) in list">
+            {{ lists.name }}.{{ lists.age }}.{{ lists.birth }}
+          </li>
+        </ul>
+      </div>
+
+      <button @click="error = !error">{{ state }}</button>
+      <p v-show="error">Message error appear in a box</p>
+      <label for="texte">Name:</label>
+      <input type="text" v-model="name" />
+      <label for="texte">Last name:</label>
+      <input type="text" v-model="lastname" />
+      <lottie-animation path="icons/arrow.json" />
       <div class="links">
         <a
           href="https://nuxtjs.org/"
@@ -37,9 +85,72 @@ export default {
     LottieAnimation
   },
   data() {
-    return {};
+    return {
+      
+
+      personnage:["mario","zelda","pokemon"],
+      list: [
+        { name: "kellig", age: 33, birth: "21.08.87" },
+        { name: "Eadrin", age: 12, birth: "27.10.2008" }
+      ],
+      error: false,
+      state: "ouvrir",
+      active: false,
+      intro: "Je vous accompagne dans cette aventure collaborative et créative",
+      name: "Kellig",
+      start: '<a href="/services">Commencer l expérience </a>',
+      age: 25,
+      x: 0,
+      y: 0,
+      name: "",
+      lastname: "",
+      grow: 100,
+      ballon: 10,
+      end: false
+    };
   },
-  methods: {},
+
+  methods: {
+
+    blow: function(){
+
+      this.grow -= 20;
+      this.ballon += 10;
+      if(this.grow <= 0){
+        this.end = true
+      }
+    },
+    restart: function(){
+      this.grow = 100;
+      this.ballon = 10;
+      this.end = false
+    },
+    clickHello: function() {
+      this.active = !this.active;
+    },
+    click: function() {
+      alert("live");
+    },
+    add: function(inc) {
+      this.age += inc;
+    },
+    minus: function(dec) {
+      this.age -= dec;
+    },
+    good: function(time) {
+      //this = à le render vue  et 'name' la data
+      return "Good " + time + " " + this.name;
+    },
+    presentation: function() {
+      return "Nous sommes tous Designers";
+    },
+    //on track offset de la mouse on move = event( paramêtre de la fonction)
+    updateXY: function(event) {
+      console.log(event);
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    }
+  },
   mounted() {}
 };
 </script>
