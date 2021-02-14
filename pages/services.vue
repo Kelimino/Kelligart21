9158b3ff00c624c343b70f4cfe1dc71004288125
kelimino <jourdren.kellig@gmail.com>
2021-02-14 16:18:27 +0100

    init

--- pages/services.vue
+++ pages/services.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
-    <h1 class="title">Services</h1>
-    <p>This a page on services</p>
+    <section class="header">
+      <h1 class="title">Services</h1>
+      <p>This a page on services</p>
+      <button ref= "btn" @click="btnE" class="btnB">Hello there </button>
+    </section>
+    <section class="domain">
+     <h2 class="DA">Direction Artistique</h2>
+      <p>This a page on services</p>
+
+    </section>
+        <section class="colors">
+     <div class="orange"><p>Hello</p></div>
+    <div class="blue"><p>Hello</p></div>
+    <div class="green"><p>Hello</p></div>
+
+    </section>
   </div>
 </template>
 
@@ -17,9 +31,26 @@ export default {
       meta: [{ name: "description", content: "Cest ma page de services" }]
     };
   },
+
+
+  methods: {
+   
+   
+  btnE: function(){
+    gsap.to(".btnB", 1, {
+      x: 200,
+      rotation: 90
+    })
+
+  }
+
+
+  },
   mounted() {
     let Overlay = document.querySelector(".title");
-    this.tl = new TimelineMax({}).fromTo(
+    this.tl = new TimelineMax({})
+    
+    .from(
       Overlay,
       1,
       {
@@ -27,14 +58,54 @@ export default {
         color: "#FF0000",
         autoAlpha: 0,
         ease: Power2.easeinOut
-      },
-      {
+      })
+      .to(Overlay,
+      1,{
         y: 0,
         color: "#000",
         autoAlpha: 1,
         ease: Power2.easeinOut
       }
     );
+
+this.tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".domain",
+    start: "100px 80%",
+    end: "400px 100%",
+    toggleActions: "restart pause resume none",
+    scrub: 2,
+    pin: ".domain p",
+    background: "#FFA500",
+    duration: 2
+  }
+})
+.to(".domain", {background: "#FFA500" })
+.from(".domain h2", {autoAlpha: 0,y: 100, duration: 1}, "-= 1")
+.to(".domain h2", {autoAlpha: 1,y: 0, duration: 1}, "-= 1");
+
+let tl3 = gsap.timeline()
+
+tl3
+.from(".orange", {xPercent: -100})
+.from(".blue", {xPercent: 100})
+.from(".green", {yPercent: -100})
+
+
+ScrollTrigger.create({
+  animation: tl3,
+  trigger: ".colors",
+  pin: true,
+  start:"top center",
+  end: "bottom 500px",
+  markers: true,
+  pinSpacing: false,
+  anticipatePin: 1,
+  toggleClass: ".activeP",
+  onEnter: () => console.log("heeeyye")
+})
+
+
   }
 };
 </script>
