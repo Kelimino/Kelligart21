c88b92604a57acc0fe43f98d3cb62b2294d45449
kelimino <jourdren.kellig@gmail.com>
2021-03-03 15:02:04 +0100

    init

--- mixins/locomotive.js
+++ mixins/locomotive.js
@@ -9,16 +9,16 @@ export default {
     mounted() {
       this.$nextTick(
         function() {
-        this.lmS = new this.locomotiveScroll({
+    this.lmS = new this.locomotiveScroll({
             el: document.querySelector(".smooth-scroll"),
             smooth: true
           });
         }.bind(this)
       );
+      
     },
     destroyed() {
       this.lmS.destroy();
     },
-  
   
   };
--- pages/accompagnement.vue
+++ pages/accompagnement.vue
@@ -100,7 +100,32 @@ gsap.registerPlugin(ScrollTrigger);
 export default {
   mixins: [locomotive],
 
+  watch:{
+
+
+  },
+
   mounted() {
+//     this.lmS.on("scroll", ScrollTrigger.update);
+//     ScrollTrigger.scrollerProxy(".smooth-scroll", {
+//       scrollTop(value) {
+//         return arguments.length ? this.lmS.scrollTo(value, 0, 0) : this.lmS.scroll.instance.scroll.y;
+//       }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+//       getBoundingClientRect() {
+//         return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+//       },
+//       // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+//       pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
+//     });
+
+//     ScrollTrigger.addEventListener("refresh", () => this.lmS.update());
+
+// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+// ScrollTrigger.refresh();
+
+
+
+
     gsap.utils.toArray(".panel").forEach(function(el) {
       gsap
         .timeline({
@@ -109,7 +134,6 @@ export default {
             start: "20% center",
             end: "bottom center",
             toggleActions: "restart none none reset",
-            scroller: ".smooth-scroll",
             scrub: true
           }
         })
