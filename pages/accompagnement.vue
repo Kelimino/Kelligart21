63fde5519e9cdd20e1e5cbf7c57a3db20c1659fb
kelimino <jourdren.kellig@gmail.com>
2021-03-12 15:59:32 +0100

    init

--- pages/accompagnement.vue
+++ pages/accompagnement.vue
@@ -21,16 +21,16 @@
     </section>
 
     <div class="story h-full">
-      <section v-for="(story, index) in stories" :key="index" class="h-full">
-        <div class="pages grid grid-cols-2 h-5/6">
+      <section
+        v-for="(story, index) in stories"
+        :key="index"
+        class="h-full"
+        :class="story.class"
+      >
+        <div class="page-wrapper grid grid-cols-2 h-5/6">
           <div
-            class="illustration border border-solid border-primary border-opacity-30 flex justify-center items-center relative"
+            class="page illustration border border-solid border-primary border-opacity-30 flex justify-center items-center relative"
           >
-            <p
-              class="absolute top-5 left-5 font-title text-primary text-xl font-bold"
-            >
-              01
-            </p>
             <div class="imgwrap w-1/2 h-2/3 mx-auto relative">
               <img
                 :src="story.path"
@@ -40,13 +40,8 @@
             </div>
           </div>
           <div
-            class="chapitre border border-solid border-primary border-opacity-30 flex flex-col justify-center items-start relative p-40 text-left"
+            class="page chapitre border border-solid border-primary border-opacity-30 flex flex-col justify-center items-start relative p-40 text-left"
           >
-            <p
-              class="absolute top-5 left-5 font-title text-primary text-xl font-bold"
-            >
-              02
-            </p>
             <h2 class="font-title text-primary text-2xl font-bold">
               {{ story.titre }}
             </h2>
@@ -66,74 +61,6 @@
           </h3>
         </div>
       </section>
-      <!-- <section class="intro panel">
-        <h1><span>nous sommes tous</span></h1>
-        <h2><span>Designers</span></h2>
-        <p>Je vous accompagne dans cette aventure collective et créative</p>
-      </section>
-
-      <section class="synopsis panel">
-        <div class="img-wrap">
-          <img src="~/assets/img/story1.jpg" alt="img " />
-        </div>
-        <h5>01</h5>
-        <h4>Synopsis</h4>
-        <h3>
-          Tout commence par une histoire où l’utilisateur en est le principal
-          protagoniste, dans un univers où son expérience est le fil conducteur
-          de votre scénario
-        </h3>
-        <p class="question">Qui est votre héros ?</p>
-      </section>
-      <section class="univers panel">
-        <div class="img-wrap">
-          <img src="~/assets/img/story2.jpg" alt="img " />
-        </div>
-        <h5>02</h5>
-        <h4>Univers</h4>
-        <h3>
-          Dans votre univers, il va découvrir un nouveau monde à appréhender.
-          L'aimera t-il ?
-        </h3>
-        <p class="question">À quoi ressemble votre univers ?</p>
-      </section>
-      <section class="declenchement panel">
-        <div class="img-wrap">
-          <img src="~/assets/img/story3.jpg" alt="img " />
-        </div>
-        <h5>03</h5>
-        <h4>Déclenchement</h4>
-        <h3>
-          Le héros débarque dans votre monde avec des attentes, motivé à
-          poursuivre sa quête
-        </h3>
-        <p class="question">Pourquoi le héros arrive t-il dans votre monde ?</p>
-      </section>
-      <section class="aventure panel">
-        <div class="img-wrap">
-          <img src="~/assets/img/story5.jpg" alt="img " />
-        </div>
-        <h5>04</h5>
-        <h4>Aventure</h4>
-        <h3>
-          Son aventure séquentielle, va le mener sur différentes étapes. Un
-          parcours itératif qu'il va naviguer. Surmonter des obstacles, trouver
-          des opportuntiés pour s'orienter et réussir son but
-        </h3>
-        <p class="question">Quel sera son parcours, que recherche t'il ?</p>
-      </section>
-      <section class="denouement panel">
-        <div class="img-wrap">
-          <img src="~/assets/img/story4.jpg" alt="img " />
-        </div>
-        <h5>05</h5>
-        <h4>Dénouement</h4>
-        <h3>
-          Une fois la mission accomplie ou même en cas d'échec, le héros en
-          sortira grandit, reviendra t-il ?
-        </h3>
-        <p class="question">Quelle est son expérience finale ?</p>
-      </section> -->
     </div>
   </div>
 </template>
@@ -153,6 +80,7 @@ export default {
       stories: [
         {
           titre: "Synopsis",
+          class: "synopsis",
           texte:
             "Tout commence par une histoire où l’utilisateur en est le principal protagoniste dans un univers où son expérience est le fil conducteur de votre scénario",
           path: require("@/assets/img/projet-back.png"),
@@ -160,10 +88,35 @@ export default {
         },
         {
           titre: "Univers",
+          class: "univers",
           texte:
             "Dans votre univers, il va découvrir un nouveau monde à appréhender. L'aimera t-il ?",
           path: require("@/assets/img/projet-back.png"),
           question: "À quoi ressemble votre univers ?"
+        },
+        {
+          titre: "Déclenchement",
+          class: "declenchement",
+          texte:
+            "Le héros débarque dans votre monde avec des attentes, motivé à poursuivre sa quête",
+          path: require("@/assets/img/projet-back.png"),
+          question: "Pourquoi est-il là ?"
+        },
+        {
+          titre: "Aventure",
+          class: "aventure",
+          texte:
+            "Son aventure séquentielle, va le mener sur différentes étapes. Un parcours itératif qu'il va naviguer. Surmonter des obstacles, trouver des opportuntiés pour s'orienter et réussir son but",
+          path: require("@/assets/img/projet-back.png"),
+          question: "Que recherche t'il ?"
+        },
+        {
+          titre: "Dénouement",
+          class: "denouement",
+          texte:
+            "Une fois la mission accomplie ou même en cas d'échec, le héros en sortira grandit, reviendra t-il ?",
+          path: require("@/assets/img/projet-back.png"),
+          question: "Quelle est son expérience finale ?"
         }
       ]
     };
@@ -174,6 +127,14 @@ export default {
     // gsap.utils.toArray(".panel").forEach(function(el) {
     let text = document.querySelector(".Dsg");
     text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+
+    let pages = document.querySelectorAll(".page");
+
+    Array.from(pages).forEach(function(page) {
+      const Number = document.createElement("span");
+      page.appendChild(Number);
+      Number.textContent = "00";
+    });
   },
 
   methods: {
