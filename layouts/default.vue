57bbef4916354014be1b7d2bc490f03b1f4e50ba
kelimino <jourdren.kellig@gmail.com>
2021-04-12 15:42:27 +0200

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,25 @@
 <template>
-  <div id="scroll" class="bg-back">
+  <div>
     <div
       class="cursor w-5 h-5 border border-primary border-solid fixed transform -translate-x-1/2 -translate-y-1/2 z-50 rounded-full pointer-events-none"
     ></div>
     <Nav />
-    <Nuxt />
+    <div id="scroll" class="bg-back">
+      <Nuxt />
+    </div>
   </div>
 </template>
 
 <script>
 import Nav from "/components/nav";
 import { cursor } from "~/mixins/mouse.js";
-// import { scrollY } from "~/mixins/scroll.js";
+// import scroll from "~/mixins/scroll.js";
+
 export default {
   name: "App",
   components: {
-    Nav,
+    Nav
   },
-  mixins: [cursor],
-  data() {
-    return {
-      lmS: null,
-    };
-  },
-  mounted() {
-    this.lmS = new this.locomotiveScroll({
-      el: document.querySelector("#scroll"),
-      smooth: true,
-    });
-  },
+  mixins: [cursor]
 };
 </script>
