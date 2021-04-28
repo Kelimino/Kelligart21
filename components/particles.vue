bee19766d2591e44e2253bd02ad61a68ee70fa17
kelimino <jourdren.kellig@gmail.com>
2021-04-28 15:56:44 +0200

    init

--- components/particles.vue
+++ components/particles.vue
@@ -1,40 +1,47 @@
 <template>
-  <div id="canvas"></div>
+  <div></div>
 </template>
 <script>
 import Matter from "matter-js/build/matter.js";
+// import { Box } from "mixins/box.js";
 export default {
   mounted() {
-    const script = function(p5) {
-      //VARIABLES
+    // this.$nextTick( () =>{
+    // });
 
+    const script = function(p5) {
+      //MODULE ALIAS
       var Engine = Matter.Engine,
         // Render = Matter.Render,
-        // Runner = Matter.Runner,
+        Runner = Matter.Runner,
         Bodies = Matter.Bodies,
         World = Matter.World;
-      // Composite = Matter.Composite;
+
+      var engine, world, box, ground;
 
       // SETUP
 
-      var engine, world, box;
       p5.setup = () => {
-        var canvas = p5.createCanvas(500, 500);
-        canvas.parent("canvas");
+        p5.createCanvas(400, 400);
 
         engine = Engine.create();
         world = engine.world;
+        Runner.run(engine);
 
-        Engine = run(engine);
-        p5.background(0);
+        box = Bodies.rectangle(200, 100, 40, 40);
+        p5.fill(0, 255, 0);
 
-        box = Bodies.rectangle(400, 200, 80, 80);
+        ground = Bodies.rectangle(100, 400, 400, 40, { isStatic: true });
+        p5.fill(255, 0, 0);
+
+        World.add(world, [box, ground]);
       };
 
       // DRAW
       p5.draw = () => {
-        p5.ellipse(100, 100, 50, 50);
-        p5.fill(255, 0, 0);
+        p5.background(0);
+        p5.rect(box.position.x, box.position.y, 40, 40);
+        p5.rect(ground.position.x, ground.position.y, 400, 400);
       };
     };
 
